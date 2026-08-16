07d821631332f3d91756a98b16b99a33fcbbe559
ANUKULI SATISH <satishanukuli0@gmail.com>
2026-08-16 15:31:49 +0530

    Time: 45 ms (80.15%), Space: 30.4 MB (88.55%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.py
+++ 2029-stone-game-ix/2029-stone-game-ix.py
@@ -1,10 +1,10 @@
 class Solution:
     def stoneGameIX(self, stones: List[int]) -> bool:
-        cnt = [0, 0, 0]
+        c = [0, 0, 0]
         for x in stones:
-            cnt[x % 3] += 1
-        if cnt[1] == 0 and cnt[2] == 0:
+            c[x % 3] += 1
+        if c[1] == 0 and c[2] == 0:
             return False
-        if cnt[0] % 2 == 0:
-            return cnt[1] > 0 and cnt[2] > 0
-        return abs(cnt[1] - cnt[2]) > 2
+        if c[0] % 2 == 0:
+            return c[1] > 0 and c[2] > 0
+        return abs(c[1] - c[2]) > 2
